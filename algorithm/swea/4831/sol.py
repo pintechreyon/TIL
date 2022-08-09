997ecc2d71f2b4421c59a60640f5141c71fb014d
pintechreyon <a0511a25@gmail.com>
2022-08-09 23:03:58 +0900

    220809

--- algorithm/swea/4831/sol.py
+++ algorithm/swea/4831/sol.py
@@ -5,18 +5,25 @@ sys.stdin = open("input.txt", "r")
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for tc in range(1, T + 1):
-    can_go, last, charger = list(map(int, input().split()))
+    go, last, charge = list(map(int, input().split()))
     c_list = list(map(int, input().split()))
-    #c_list[i] + can_go < c_list[i+1}이면 0으로 환산
-    for i in range(charger):
-        if i < range(charger)[-1]:
-            if c_list[i+1] > can_go + c_list[i]:
-                result = 0
-                break
-        else:
-            result = int((last - c_list[0]) / can_go)
-            break
-        if c_list[-1] + can_go < last:
-            result = 0
 
-    print(f'#{tc} {result}')
+    cnt = 0
+    pos = 0
+    run = 0+go
+    station = [0]*last
+    for i in c_list:
+        station[i] += 1
+
+    while run < last:
+        if pos == run:
+            cnt = 0
+            break
+        elif station[run] == 1:
+            pos = run
+            run = pos + go
+            cnt += 1
+        else:
+            run -= 1
+    print(f'#{tc} {cnt}')
+        
