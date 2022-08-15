76dfd750c3a53a7611f051d077d7d974081fa163
pintechreyon <a0511a25@gmail.com>
2022-08-15 22:38:33 +0900

    220815

--- algorithm/swea/2001/sol.py
+++ algorithm/swea/2001/sol.py
@@ -7,10 +7,11 @@ for tc in range(1, T+1):
     num = list(map(int, input().split()))
     N1 = num[0]
     N2 = num[1]
+    M = N2-1
     arr = [list(map(int, input().split())) for _ in range(N1)]
     result = 0
-    for i in range(N1 - N2 - 1):
-        for j in range(N1 - N2 - 1):
+    for i in range(N1 - M):
+        for j in range(N1 - M):
             max_V=0
             for k in range(N2):
                 for l in range(N2):
