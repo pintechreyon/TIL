0478c067d0920aade1b34bc81cf9756dbf42627a
pintechreyon <a0511a@naver.com>
2022-09-12 15:50:45 +0900

    220912

---
+++ algorithm/swea/14557/sol.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1,T+1):
+    arr = list(input())
+    cnt = 0
+    result = ''
+    for i in arr:
+        if i == '1':
+            cnt += 1
+    if cnt % 2 == 1:
+        result = 'yes'
+    else:
+        result = 'no'
+    print(f'#{tc} {result}')
+
+
