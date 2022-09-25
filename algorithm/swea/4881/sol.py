2cf9a969e9e4e633645fd3d375247321f98b8728
pintechreyon <a0511a@naver.com>
2022-09-26 02:43:43 +0900

    220926

---
+++ algorithm/swea/4881/sol.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('input.txt')
+
+def DFS(idx, total):
+    global min_v
+    if idx == N:
+        if total < min_v:
+            min_v = total
+        return
+    if total > min_v:
+        return
+
+    for i in range(N):
+        if i not in visited:
+            visited.append(i)
+            DFS(idx+1, total+arr[idx][i])
+            visited.pop()
+
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    visited = []
+    min_v = 100
+    DFS(0, 0)
+    print(f'#{tc} {min_v}')
