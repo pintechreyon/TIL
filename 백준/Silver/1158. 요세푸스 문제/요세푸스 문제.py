218ba41584ebee22e65ff1056bc6b3cbe4a86833
pintechreyon <108873348+pintechreyon@users.noreply.github.com>
2022-09-29 09:30:52 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+arr = list(range(1, N+1))
+var = 0
+result = []
+for i in range(N):
+    var += M-1
+    if var >= len(arr):
+        var = var % len(arr)
+    result.append(str(arr.pop(var)))
+
+print(f'<{", ".join(result)}>')
