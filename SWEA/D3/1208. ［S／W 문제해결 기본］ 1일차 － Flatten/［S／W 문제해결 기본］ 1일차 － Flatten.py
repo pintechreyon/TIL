f4cd947a1fcffa2ea1e900111442da2532bd6299
pintechreyon <108873348+pintechreyon@users.noreply.github.com>
2022-09-29 10:20:10 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 266 ms, Memory: 62,800 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -0,0 +1,16 @@
+for tc in range(1,11):
+    dump = int(input())
+    arr = list(map(int, input().split()))
+    for i in range(dump):
+        for j in range(len(arr)):
+            if arr[j] == min(arr):
+                arr[j] = arr[j]+1
+                break
+
+    for i in range(dump):
+        for j in range(len(arr)):
+            if arr[j] == max(arr):
+                arr[j] = arr[j]-1
+                break
+    result = max(arr)-min(arr)
+    print(f'#{tc} {result}')
