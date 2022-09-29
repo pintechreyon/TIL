cff97bc8f5744eacfa10423e89041bcf6234e6db
pintechreyon <108873348+pintechreyon@users.noreply.github.com>
2022-09-29 10:19:49 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 202 ms, Memory: 62,820 KB -BaekjoonHub

---
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -0,0 +1,23 @@
+T = 10
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = []
+    for i in range(N):
+        arr.append(list(map(int, input().split())))
+    cnt = 0
+    for i in range(N):
+        j = 0
+        while j < N:
+            if arr[j][i] == 1:
+                k = j + 1
+                while k < N:
+                    if arr[k][i] == 2:
+                        cnt += 1
+                        break
+                    else:
+                        k += 1
+                j = k + 1
+            else:
+                j += 1
+    print(f'#{tc} {cnt}')
