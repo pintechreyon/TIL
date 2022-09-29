1e1648fcaab54599796d147f68b181ece11197e6
pintechreyon <108873348+pintechreyon@users.noreply.github.com>
2022-09-29 09:32:58 +0900

    [Silver IV] Title: 올바른 배열, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -0,0 +1,20 @@
+N = int(input())
+arr = []
+result = 0
+for i in range(N):
+    arr.append(int(input()))
+for i in arr:
+    cnt = 0
+    if i in arr:
+        cnt += 1
+    if i+1 in arr:
+        cnt += 1
+    if i+2 in arr:
+        cnt += 1
+    if i+3 in arr:
+        cnt += 1
+    if i+4 in arr:
+        cnt += 1
+    if cnt > result:
+        result = cnt
+print(5-result)
