e28421bac52f4c19edbafaf338f25efc4aa1b9cd
pintechreyon <108873348+pintechreyon@users.noreply.github.com>
2022-09-29 09:30:33 +0900

    [Silver I] Title: 팔, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,11 @@
+N, M = input().split()
+result = 0
+if len(N) == len(M):
+    for i in range(len(N)):
+        if N[i] == '8' and M[i] == '8':
+            result += 1
+        elif N[i] == M[i]:
+            continue
+        else:
+            break
+print(result)
