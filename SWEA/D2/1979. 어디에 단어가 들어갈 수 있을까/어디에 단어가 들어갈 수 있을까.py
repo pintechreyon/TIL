3675d89b6ddad0bac6911ddc357e538abbe56d9b
pintechreyon <108873348+pintechreyon@users.noreply.github.com>
2022-09-29 10:24:00 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 150 ms, Memory: 59,092 KB -BaekjoonHub

---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -0,0 +1,36 @@
+T = int(input())
+for tc in range(1,T+1):
+    N,M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    result = 0
+    for i in range(N):
+        cnt = 0
+        for j in range(N):
+            if arr[i][j] == 0:
+                if cnt == M:
+                    result += 1
+                    cnt = 0
+                else:
+                    cnt = 0
+            else:
+                cnt +=1
+            if j == N-1:
+                if cnt == M:
+                    result += 1
+    for i in range(N):
+        cnt = 0
+        for j in range(N):
+            if arr[j][i] == 0:
+                if cnt == M:
+                    result += 1
+                    cnt = 0
+                else:
+                    cnt = 0
+            else:
+                cnt += 1
+            if j == N-1:
+                if cnt == M:
+                    result += 1
+    print(f'#{tc} {result}')
+
+
