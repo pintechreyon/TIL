da8443050859dc359717f07c58505da0bd3ec029
pintechreyon <108873348+pintechreyon@users.noreply.github.com>
2022-09-29 09:35:15 +0900

    [Bronze III] Title: 오븐 시계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,10 @@
+H, M = map(int, input().split())
+num = int(input())
+if M + num >= 60:
+    H = H + (M + num) // 60
+    M = (M + num) % 60
+else:
+    M = M + num
+if H >= 24:
+    H = H % 24
+print(f'{H} {M}')
